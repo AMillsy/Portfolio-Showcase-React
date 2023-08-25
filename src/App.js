2c48a84e575b953565916c0f55a57bdcd64f26dc
AMillsy <00adammills00@gmail.com>
2023-08-25 13:44:59 +0100

    Made a home page splitter for each section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
 import "./styles/Styles.css";
-
 import Layout from "./pages/Layout";
+import Splitter from "./components/Splitter";
+import portfolioImage1 from "./images/portfolioSplit1.png";
+import portfolioImage2 from "./images/portfolioSplit2.png";
 
 function App() {
-  return <Layout></Layout>;
+  const splitters = [
+    {
+      name: "Portfolio",
+      backgroundColour: "grey",
+      splitImage1: portfolioImage1,
+      splitImage2: portfolioImage2,
+    },
+    {
+      name: "Resume",
+      backgroundColour: "#210124",
+      splitImage1: portfolioImage1,
+      splitImage2: portfolioImage2,
+    },
+    {
+      name: "Contacts",
+      backgroundColour: "#210124",
+      splitImage1: portfolioImage1,
+      splitImage2: portfolioImage2,
+    },
+    {
+      name: "About me",
+      backgroundColour: "#210124",
+      splitImage1: portfolioImage1,
+      splitImage2: portfolioImage2,
+    },
+  ];
+
+  return (
+    <Layout>
+      {splitters.map(
+        ({ name, backgroundColour, splitImage1, splitImage2 }, i) => {
+          return (
+            <Splitter
+              name={name}
+              backgroundColour={backgroundColour}
+              splitImage1={splitImage1}
+              splitImage2={splitImage2}
+              currentSection={i}
+            />
+          );
+        }
+      )}
+    </Layout>
+  );
 }
 
 export default App;
--- src/components/Splitter.js
+++ src/components/Splitter.js
@@ -1,9 +1,70 @@
-const Splitter = ({ name }) => {
-  return (
-    <section>
-      <h2>{name}</h2>
-    </section>
-  );
+import "../styles/Splitter.css";
+
+const Splitter = ({
+  name,
+  backgroundColour,
+  splitImage1,
+  splitImage2,
+  currentSection,
+}) => {
+  function displayCorrectSection() {
+    const sectionType = currentSection % 2;
+    console.log(sectionType);
+    if (sectionType === 0) {
+      return (
+        <>
+          <div
+            className="splitLeft"
+            style={{
+              background: backgroundColour,
+              borderTopLeftRadius: "2rem",
+            }}
+          >
+            <h2 className="splitHeader">{name}</h2>
+            <p></p>
+          </div>
+          <div className="splitRight" style={{ borderTopRightRadius: "2rem" }}>
+            <img
+              src={splitImage1}
+              className="splitterImage"
+              style={{ borderTopRightRadius: "2rem" }}
+            ></img>
+            <img
+              src={[splitImage2]}
+              className="splitterImageHover"
+              style={{ borderTopRightRadius: "2rem" }}
+            ></img>
+          </div>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <div className="splitRight">
+          <img
+            src={splitImage1}
+            className="splitterImage"
+            style={{ borderTopLeftRadius: "2rem" }}
+          ></img>
+          <img
+            src={[splitImage2]}
+            className="splitterImageHover"
+            style={{ borderTopLeftRadius: "2rem" }}
+          ></img>
+        </div>
+        <div
+          className="splitLeft"
+          style={{ background: backgroundColour, borderTopRightRadius: "2rem" }}
+        >
+          <h2 className="splitHeader">{name}</h2>
+          <p></p>
+        </div>
+      </>
+    );
+  }
+
+  return <section className="splitter">{displayCorrectSection()}</section>;
 };
 
 export default Splitter;
