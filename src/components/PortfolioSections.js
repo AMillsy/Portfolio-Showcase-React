b97359c68db1387b763d9e6dc6787ae6218c0c01
AMillsy <00adammills00@gmail.com>
2023-08-26 20:52:40 +0100

    making dynamic cards for the portfolio

--- src/components/PortfolioSections.js
+++ src/components/PortfolioSections.js
@@ -1,11 +1,31 @@
-import portfolioImage from "../images/portfolioSplit1.png";
+import externalLink from "../images/externalLink.png";
+import githubImage from "../images/github.png";
 import "../styles/PortfolioSection.css";
-const PortfolioSection = () => {
+const PortfolioSection = ({
+  imageSrc,
+  title,
+  date,
+  externalSrc,
+  githubLink,
+}) => {
   return (
-    <a className="portfolioCard">
-      <h2>Find that Place</h2>
-      <h3>Date created</h3>
-      <img className="portfolioImage" src={portfolioImage}></img>
+    <a className="portfolioCard" href="#">
+      <img className="portfolioImage" src={imageSrc} alt={title}></img>
+      <div className="portfolioDescription">
+        <div className="cardDesc">
+          <h2 className="cardTitle">{title}</h2>
+          <h3 className="cardDate">{date}</h3>
+
+          <div className="linkContainer">
+            <a href={externalSrc}>
+              <img src={externalLink} className="links"></img>
+            </a>
+            <a href={githubLink}>
+              <img src={githubImage} className="links"></img>
+            </a>
+          </div>
+        </div>
+      </div>
     </a>
   );
 };
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,33 @@
 import PortfolioSection from "../components/PortfolioSections";
+import visualVault from "../images/portfolioSplit2.png";
 
 const Portfolio = () => {
+  const portfolioCards = [
+    {
+      imageSrc: visualVault,
+      title: "Visual Vault",
+      date: "10th July 2023",
+      externalSrc: "https://visual-vault-70f24eab589d.herokuapp.com/",
+      githubLink: "https://github.com/AMillsy/Visual-Vault",
+    },
+  ];
+
   return (
     <>
-      <p>Portfolio page</p>
-      <PortfolioSection />
+      <h2>Projects</h2>
+      {portfolioCards.map(
+        ({ imageSrc, title, date, externalSrc, githubLink }) => {
+          return (
+            <PortfolioSection
+              imageSrc={imageSrc}
+              title={title}
+              date={date}
+              externalSrc={externalSrc}
+              githubLink={githubLink}
+            />
+          );
+        }
+      )}
     </>
   );
 };
