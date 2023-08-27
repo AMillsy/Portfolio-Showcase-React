9eae9539ec3333ddd99090095b1dbb49e3e4e71e
AMillsy <00adammills00@gmail.com>
2023-08-27 15:50:33 +0100

    useState in contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,38 +1,53 @@
 import "../styles/Contact.css";
-
+import { useState } from "react";
 const Contact = () => {
+  const [data, setData] = useState({ name: "", email: "", message: "" });
+
+  const changeData = (e) => {
+    const { name, value } = e.target;
+
+    setData({ ...data, [name]: value });
+    return;
+  };
   return (
     <section className="contactSection">
       <h2 className="contactTitle">Contact</h2>
 
       <div className="formContainer">
         <div>
-          <label className="labelName" for="fname">
-            Your name
-          </label>
+          <label className="labelName">Your name</label>
           <input
             type="text"
             id="name"
-            name="firstname"
+            name="name"
             placeholder="Your name.."
+            onChange={(e) => changeData(e)}
+            value={data.name}
           ></input>
         </div>
         <div>
-          <label className="labelEmail" for="email">
-            Your email
-          </label>
+          <label className="labelEmail">Your email</label>
           <input
             type="text"
             id="email"
             name="email"
             placeholder="JohnDoe@gmail.com"
+            onChange={(e) => changeData(e)}
           ></input>
         </div>
         <div>
-          <label for="message">Message</label>
-          <textarea id="message" name="message"></textarea>
+          <label>Message</label>
+          <textarea
+            id="message"
+            name="message"
+            onChange={(e) => changeData(e)}
+          ></textarea>
         </div>
-        <button className="btnMessage">Send Message</button>
+        <button className="btnMessage" onClick={() => console.log(data)}>
+          Send Message
+        </button>
+
+        <p className="error"></p>
       </div>
     </section>
   );
