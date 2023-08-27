305fa6c36e001baa5c106edabe86e014c2356ce9
AMillsy <00adammills00@gmail.com>
2023-08-27 15:34:33 +0100

    stylised the contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,26 +6,32 @@ const Contact = () => {
       <h2 className="contactTitle">Contact</h2>
 
       <div className="formContainer">
-        <label className="labelName" for="fname">
-          Your name
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="firstname"
-          placeholder="Your name.."
-        ></input>
-        <label className="labelEmail" for="email">
-          Your email
-        </label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          placeholder="JohnDoe@gmail.com"
-        ></input>
-        <label for="message">Message</label>
-        <textarea id="message" name="message"></textarea>
+        <div>
+          <label className="labelName" for="fname">
+            Your name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="firstname"
+            placeholder="Your name.."
+          ></input>
+        </div>
+        <div>
+          <label className="labelEmail" for="email">
+            Your email
+          </label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="JohnDoe@gmail.com"
+          ></input>
+        </div>
+        <div>
+          <label for="message">Message</label>
+          <textarea id="message" name="message"></textarea>
+        </div>
         <button className="btnMessage">Send Message</button>
       </div>
     </section>
