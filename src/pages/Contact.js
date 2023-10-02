0bfc3357bd6788a9fd0be785e777d87bdc8841e9
AMillsy <00adammills00@gmail.com>
2023-10-02 17:38:06 +0100

    clearing forms properly

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,21 @@
 import "../styles/Contact.css";
-import { useState, useEffect } from "react";
-
+import { useState, useEffect, useRef } from "react";
+import emailjs, { send } from "@emailjs/browser";
 const Contact = () => {
+  const form = useRef();
   useEffect(() => {});
-  const [data, setData] = useState({ name: "", email: "", message: "" });
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    message: "",
+    subject: "",
+  });
 
   const [errorList, setErrorList] = useState();
+  const [successMessage, setSuccess] = useState({
+    message: "",
+    color: "green",
+  });
   let errorMessage = [];
 
   const changeData = (e) => {
@@ -18,10 +28,13 @@ const Contact = () => {
   const checkData = () => {
     errorMessage = [];
     setErrorList(null);
-    if (!(data.name || data.email || data.message)) return;
+    if (!(data.name || data.email || data.message || data.subject)) return;
 
     const re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
+    if (!data.subject) {
+      errorMessage.push("Please Enter a subject");
+    }
     if (!data.name) {
       errorMessage.push("Please Enter name");
     }
@@ -34,7 +47,12 @@ const Contact = () => {
       errorMessage.push("Please enter a message");
     }
 
-    displayError(errorMessage);
+    if (errorMessage.length !== 0) {
+      displayError(errorMessage);
+      return false;
+    }
+
+    return true;
   };
 
   const displayError = () => {
@@ -45,13 +63,47 @@ const Contact = () => {
     );
     console.log(errorList);
   };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (!checkData()) return;
+    emailjs
+      .send("service_zy45bk3", "template_gw68jha", data, "1oDfEvi6N6zEOk7PH")
+      .then(
+        async (result) => {
+          setSuccess({ message: "Successfully sent email", color: "green" });
+          setData((prevState) => ({
+            ...prevState,
+            name: "",
+            email: "",
+            subject: "",
+            message: "",
+          }));
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+          setSuccess({ message: "Error sending email", color: "red" });
+        }
+      );
+
+    setTimeout(function () {
+      setSuccess({ message: "", color: "green" });
+    }, 2000);
+  };
   return (
     <section className="contactSection">
       <h2 className="contactTitle">Contact</h2>
 
-      <div className="formContainer" onBlur={() => checkData()}>
+      <form
+        className="formContainer"
+        ref={form}
+        onBlur={() => checkData()}
+        onSubmit={sendEmail}
+      >
         <div>
-          <label className="labelName">Your name</label>
+          <label className="labelName">Your name:</label>
           <input
             type="text"
             id="name"
@@ -62,7 +114,7 @@ const Contact = () => {
           ></input>
         </div>
         <div>
-          <label className="labelEmail">Your email</label>
+          <label className="labelEmail">Your email:</label>
           <input
             type="text"
             id="email"
@@ -72,19 +124,30 @@ const Contact = () => {
           ></input>
         </div>
         <div>
-          <label>Message</label>
+          <label className="labelEmail">Subject:</label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            placeholder="subject"
+            onChange={(e) => changeData(e)}
+          ></input>
+        </div>
+        <div>
+          <label>Message:</label>
           <textarea
             id="message"
             name="message"
             onChange={(e) => changeData(e)}
           ></textarea>
         </div>
-        <button className="btnMessage" onClick={() => console.log(data)}>
+        <button className="btnMessage" onClick={() => sendEmail}>
           Send Message
         </button>
 
         <ul className="error">{errorList}</ul>
-      </div>
+        <p style={{ color: successMessage.color }}>{successMessage.message}</p>
+      </form>
     </section>
   );
 };
